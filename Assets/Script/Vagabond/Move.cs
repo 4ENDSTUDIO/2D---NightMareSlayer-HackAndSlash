bdc2aed2748e907481c050442d28eda85ec91b71
Rizky Hendra Saputra <RizkyHendra337@gmail.com>
2022-05-10 20:24:15 +0700

    push move ( brackey )

--- Assets/Script/Vagabond/Move.cs
+++ Assets/Script/Vagabond/Move.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    
+    public CharacterController2D controller;
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    bool jump = false;
+    Rigidbody2D rb;
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    public void Update()
+    {
+       horizontalMove= Input.GetAxisRaw("Horizontal") * runSpeed;
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+    public void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false,jump);
+        jump = false;
+    }
 
 }
